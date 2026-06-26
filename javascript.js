db4f5bcc2eaeac484ec535abbff87ba82f125bff
melnyk01 <223485602+melnyk01@users.noreply.github.com>
2026-06-26 14:34:50 +0200

    Small corrections Add links to the socials, create achievements, background and goals pages, other minor fixes.

---
+++ javascript.js
@@ -0,0 +1,7 @@
+linkedInButton = document.querySelector('#linked-in-button');
+linkedInButton.addEventListener('click', () => {
+    alert('I do not have this one yet :)')
+})
+
+// contactMeBtn = document.querySelector('#header-signup-button');
+// contactMeBtn.addEventListener('click', () => )
